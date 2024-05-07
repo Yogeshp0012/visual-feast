c86fab571f5ef6102cc47c61b11d4cd7bb73b9b6
Yogeshp0012 <yogeshp0012@gmail.com>
2024-05-07 22:50:39 +0530

    Modify homepage

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,29 +1,22 @@
-import '../styles/globals.css';
-import { Footer } from '../components/footer';
-import { Header } from '../components/header';
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
 
-export const metadata = {
-    title: {
-        template: '%s | Netlify',
-        default: 'Netlify Starter'
-    }
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "REPIX",
+  description: "Photoediting App",
 };
 
-export default function RootLayout({ children }) {
-    return (
-        <html lang="en" data-theme="lofi">
-            <head>
-                <link rel="icon" href="/favicon.svg" sizes="any" />
-            </head>
-            <body className="antialiased text-white bg-blue-900">
-                <div className="flex flex-col min-h-screen px-6 bg-grid-pattern sm:px-12">
-                    <div className="flex flex-col w-full max-w-5xl mx-auto grow">
-                        <Header />
-                        <div className="grow">{children}</div>
-                        <Footer />
-                    </div>
-                </div>
-            </body>
-        </html>
-    );
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={`${inter.className} bg-gray-900`}>{children}</body>
+    </html>
+  );
 }
