ec1784293119ff96a798fa96b6ee332a2bbb9c2b
Yogeshp0012 <yogeshp0012@gmail.com>
2024-05-07 22:55:27 +0530

    Modify homepage

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,22 +1,16 @@
-import type { Metadata } from "next";
-import { Inter } from "next/font/google";
-import "./globals.css";
+import '../styles/globals.css';
 
-const inter = Inter({ subsets: ["latin"] });
-
-export const metadata: Metadata = {
-  title: "REPIX",
-  description: "Photoediting App",
+export const metadata = {
+    title: {
+        template: '%s | REPIX',
+        default: 'REPIX'
+    }
 };
 
-export default function RootLayout({
-  children,
-}: Readonly<{
-  children: React.ReactNode;
-}>) {
-  return (
-    <html lang="en">
-      <body className={`${inter.className} bg-gray-900`}>{children}</body>
-    </html>
-  );
+export default function RootLayout({ children }) {
+    return (
+<html lang="en">
+    <body className={`${inter.className} bg-gray-900`}>{children}</body>
+</html>
+    );
 }
