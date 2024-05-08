3811297402db7eae1eab360c5a055b793481ca1e
Yogeshp0012 <yogeshp0012@gmail.com>
2024-05-08 17:52:09 +0530

    Use unsplash images

--- app/page.jsx
+++ app/page.jsx
@@ -174,7 +174,7 @@ export default function Home() {
                 </div>
             </div>
             <>
-                <div className="relative z-10" aria-labelledby="modal-title" role="dialog" aria-modal="true">
+                <div className="hidden relative z-10" aria-labelledby="modal-title" role="dialog" aria-modal="true">
                     <div className="fixed inset-0 bg-gray-900 bg-opacity-75 transition-opacity" />
                     <div className="fixed inset-0 z-10 w-screen overflow-y-auto">
                         <div className="flex min-h-full items-end justify-center p-4 text-center sm:items-center sm:p-0">
