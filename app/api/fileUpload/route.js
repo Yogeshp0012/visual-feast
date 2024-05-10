6e98705ef5ed401b36c4aa91b9ffa12d5cd3654a
Yogeshp0012 <yogeshp0012@gmail.com>
2024-05-10 15:34:02 +0530

    Test image upload

--- app/api/fileUpload/route.js
+++ app/api/fileUpload/route.js
@@ -1,5 +1,6 @@
 import fs from 'fs';
-import path from 'path';
+import { writeFile } from 'fs/promises';
+import path, { join } from 'path';
 
 const uploadFolder = path.join(process.cwd(), 'uploads');
 
@@ -11,11 +12,10 @@ export async function POST(req) {
   const data = await req.formData()
   const file = data.get("file");
   const username = data.get("username");
-  try {
-    const buffer = await file.arrayBuffer();
-    await fs.promises.writeFile(`../../../public/images/${username}/${file.name}`, Buffer.from(buffer));
-    return new Response(`File uploaded successfully`);
-  } catch (error) {
-    return new Response('Error uploading file');
-  }
+  const path = join("./", "public/images", file.name)
+  const bytes = await file.arrayBuffer();
+  const buffer = Buffer.from(bytes);
+  await writeFile(path, buffer);
+  console.log(`Open ${path}`);
+  return new Response("ok");
 }
--- app/page.jsx
+++ app/page.jsx
@@ -74,7 +74,7 @@ export default function Home() {
     }
 
     const handleFileUpload = async () => {
- 
+
     };
 
 
